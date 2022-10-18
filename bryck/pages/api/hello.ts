b9d3c56d9d43df2c22568bf6c859931c5fe7d0ba
SIRU TAN <siru0915@gmail.com>
2022-10-18 14:36:32 -0400

    Connection to MongoDB set up.

--- bryck/pages/api/hello.ts
+++ bryck/pages/api/hello.ts
@@ -1,1 +1,18 @@
-export {}
+import clientPromise from "../../lib/mongodb";
+
+
+export default async (req:any, res:any) => {
+    try {
+        const client = await clientPromise;
+        const db = client.db("nextjs-mongodb-demo");
+
+        const movies = await db
+            .collection("users")
+            .find({})
+            .toArray();
+
+        res.json(movies);
+    } catch (e) {
+        console.error(e);
+    }
+};
